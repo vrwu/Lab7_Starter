d4cccea7953eed95b5e73b34d163ec9a813e987b
Vivian Wu <vivianrwu@gmail.com>
2021-11-06 20:44:03 -0700

    add explore items

--- sw.js
+++ sw.js
@@ -9,6 +9,28 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  let urlsToCache = [
+    "/",
+    "index.html",
+    "assets/styles/main.css",
+    "assets/scripts/main.js",
+    "assets/scripts/Router.js",
+    'assets/images/icons/0-star.svg',
+    'assets/images/icons/1-star.svg',
+    'assets/images/icons/2-star.svg',
+    'assets/images/icons/3-star.svg',
+    'assets/images/icons/4-star.svg',
+    'assets/images/icons/5-star.svg',
+    'assets/images/icons/arrow-down.png',
+  ];
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +43,7 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+   event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +52,36 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+   event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
+
